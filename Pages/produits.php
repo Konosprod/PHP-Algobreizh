d3fa4a9c851324b7823dcb3b2503630abb07bd82
Nesthea <flavien.huot@gmail.com>
2016-04-12 11:23:15 +0200

    remise a niveau de certaines images
    Signed-off-by: Nesthea <flavien.huot@gmail.com>

--- Pages/produits.php
+++ Pages/produits.php
@@ -1,1 +1,17 @@
 <?php
+include_once 'Twig/Autoloader.php';
+include_once 'Classes/SPDO.php';
+
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/Templates');
+
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('produits.twig');
+
+echo $template->render(array());
+
+
+
+?>
