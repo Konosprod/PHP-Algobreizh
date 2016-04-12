c0e4388d8f983d43b49cf8f6b3de6ce87b602ec5
Konosprod <konosprod@free.fr>
2016-04-12 11:38:31 +0200

    Fix produits
    Signed-off-by: Konosprod <konosprod@free.fr>

--- Pages/produits.php
+++ Pages/produits.php
@@ -1,10 +1,10 @@
 <?php
-include_once 'Twig/Autoloader.php';
-include_once 'Classes/SPDO.php';
+include_once '../Twig/Autoloader.php';
+include_once '../Classes/SPDO.php';
 
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem(__DIR__.'/Templates');
+$loader = new Twig_Loader_Filesystem(__DIR__.'/../Templates');
 
 $twig = new Twig_Environment($loader);
 
