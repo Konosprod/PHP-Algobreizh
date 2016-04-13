d18c86eb6dcacfc52936ba1fd8ad312da6894572
Nesthea <flavien.huot@gmail.com>
2016-04-13 11:20:02 +0200

    Creation et implementation page produits
    Signed-off-by: Nesthea <flavien.huot@gmail.com>

--- Pages/produits.php
+++ Pages/produits.php
@@ -10,7 +10,23 @@ $twig = new Twig_Environment($loader);
 
 $template = $twig->loadTemplate('produits.twig');
 
-echo $template->render(array());
+$pdo = SPDO::getInstance();
+$sqlfamille = "SELECT * FROM familles";
+$stmtfamille = $pdo->query($sqlfamille);
+$famille = $stmtfamille->fetchAll(PDO::FETCH_ASSOC);
+
+$sqlproduit = "SELECT * FROM articles WHERE idFamille ='".$_GET['f']."'";
+$stmtproduit = $pdo->query($sqlproduit);
+$produit = $stmtproduit->fetchAll(PDO::FETCH_ASSOC);
+
+
+$images = array(array('src' => "../Images/images_famille/famille_pate.png"));
+
+echo $template->render(array('familles'=>$famille, 'produits'=>$produit, 'images'=>$images
+	
+));
+
+
 
 
 
