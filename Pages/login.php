637b416bcbc5d568a451ff387b94208acf2aafa6
Nesthea <flavien.huot@gmail.com>
2016-04-19 10:21:00 +0200

    Ajout de la session et ses attributs
    Signed-off-by: Nesthea <flavien.huot@gmail.com>

--- Pages/login.php
+++ Pages/login.php
@@ -1,18 +1,45 @@
 <?php
 
 include_once '../Classes/SPDO.php';
+include_once '../Classes/Panier.php';
 
+// création de session
+if (!isset($_SESSION))
+{
+	session_start();
+	$_SESSION["log"] = false;
+}
+
+
+
+// Recuperation du login et mot de passe
 if (isset ($_POST["code"]) && isset ($_POST["password"]))
 {
+	// Hash du mot de passe
 	$hash = hash('sha256', $_POST['password']);
-	$request = "SELECT * FROM utilisateurs WHERE codeClient = '".$_POST["code"]."' AND motDePasse = '".$hash."'";
 	
+	// requete de comparaison
+	$request = "SELECT * FROM utilisateurs WHERE codeClient = '".$_POST["code"]."' AND motDePasse = '".$hash."'";
 	$pdo = SPDO::getInstance();
 	$stmt = $pdo->query($request);
 	
+	// resultat
 	$res = $stmt->fetch(PDO::FETCH_ASSOC);
 	if ($res != NULL)
 	{
+		$panier = new Panier();
+		
+		$_SESSION['admin'] = false;
+		$_SESSION["log"] = true;
+		$_SESSION["panier"] = serialize($panier);
+		$_SESSION["code"] = $res['codeClient'];
+		
+		
+		if ( $res['teleprospecteur'] == 1)
+		{
+			$_SESSION['admin'] = true;
+		}
+		
 		header("Location: accueil.php");
 		die();
 	}
