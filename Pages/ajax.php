bd7c0f47f6a0477e17b92f08baa717d5f6449c9b
Konosprod <konosprod@free.fr>
2016-04-19 10:42:00 +0200

    initialisation de la session
    Signed-off-by: Konosprod <konosprod@free.fr>

--- Pages/ajax.php
+++ Pages/ajax.php
@@ -2,11 +2,17 @@
 
 include_once '../Classes/Panier.php';
 
+if(!isset($_SESSION))
+{
+	session_start();
+}
+
 if (isset($_POST['idProduit']))
 {
 	$panier = unserialize($_SESSION['panier']);
 	$panier->ajouterproduits($_POST['idProduit']);
 	$_SESSION['panier'] = serialize($panier);
+	
 	die(json_encode(array("status"=>true)));
 }
 ?>
