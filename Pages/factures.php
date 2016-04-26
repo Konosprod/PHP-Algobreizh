2d7923487aa1349652a71bb0f10716047cb5720c
Konosprod <konosprod@free.fr>
2016-04-26 12:13:54 +0200

    Update facture. Affichage du tout bien comme il faut
    Signed-off-by: Konosprod <konosprod@free.fr>

--- Pages/factures.php
+++ Pages/factures.php
@@ -22,10 +22,39 @@ $twig = new Twig_Environment($loader);
 $template = $twig->loadTemplate('factures.twig');
 
 $pdo = SPDO::getInstance();
+
 $sql = "SELECT * FROM familles";
 $stmt = $pdo->query($sql);
-
 $familles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo $template->render(array("familles"=>$familles));
+$sql = "SELECT * FROM commandes WHERE codeClient = '".$_SESSION['code']."'";
+$stmt = $pdo->query($sql);
+$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$commandes = array();
+
+foreach($res as $commande)
+{
+	$commande["details"] = array();
+	$sql = "SELECT * FROM details WHERE idCommande = '".$commande["idCommande"]."'";
+	$stmt = $pdo->query($sql);
+	$details = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$v = array();
+	
+	foreach($details as $item)
+	{
+		$nom = $pdo->query("SELECT libelleArticle FROM articles WHERE idArticle ='".$item["idArticle"]."'")->fetch(PDO::FETCH_ASSOC)["libelleArticle"];
+		$var = array();
+		
+		$var["nom"] = $nom;
+		$var["montant"] = $item["montant"];
+		$var["qte"] = $item["qteArticle"];
+		$var["total"] = $item["montant"] * $item["qteArticle"];
+		$v[] = $var;
+	}
+	
+	$commande["details"] = $v;
+	$commandes[] = $commande;
+}
+
+echo $template->render(array("commandes"=>$commandes, "familles"=>$familles));
 ?>
